76b9346a143c07a009fd1c39bc62d8565587ca54
madoubao <sdumayucong@163.com>
2018-10-30 10:59:44 +0800

    Add files via upload

---
+++ lesson_code/week2/costFunctionJ.m
@@ -0,0 +1,11 @@
+function J = costFunctionJ(X, y, theta),
+  % X is the "design matrix" containing our training examples
+  % y is the class labels
+  
+  m = size(X, 1);   % number of training examples, size of rows
+  predictions = X * theta; % predictions of hapothesis on all m examples
+  sqrErrors = (predictions - y) .^ 2; % squared errors .^ 指的是对数据中每个元素平方
+  
+  J = 1 / (2 * m) * sum(sqrErrors); 
+  
+endfunction
